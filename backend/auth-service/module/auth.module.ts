abde2eea8927767a5b2accf767fce2cd811cf30c
Передереев Илья Николаевич <peredereevin@gmail.com>
2025-04-21 13:05:51 +0300

    Delete backend/auth-service/module/auth.module.ts

--- backend/auth-service/module/auth.module.ts
+++
@@ -1,7 +0,0 @@
-import { AuthController } from "../controller/auth.controller";
-import { AuthService } from "../service/auth.service";
-
-const authService = new AuthService();
-const authController = new AuthController(authService);
-
-export { authService, authController };
